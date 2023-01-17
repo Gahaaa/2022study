f388f76b05f456ddde26b6b74c71575437458327
Gahaaa <103615847+Gahaaa@users.noreply.github.com>
2023-01-17 17:59:19 +0900

    230117 프로그래머스

---
+++ programmers/Lv.0/test20.js
@@ -0,0 +1,25 @@
+/*
+문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
+
+*/
+
+function solution(my_string, n) {
+    let text = [...my_string];
+    let text2=[];
+    text.forEach(element => {
+        text2.push(element.repeat(n));
+    });
+    console.log(text2.join(''));
+
+}
+solution('hello', 3);
+
+
+
+// function solution(my_string, n) {
+//     var answer = [...my_string].map(v => v.repeat(n)).join("");
+//     console.log(answer);
+//     return answer;
+// }
+
+
